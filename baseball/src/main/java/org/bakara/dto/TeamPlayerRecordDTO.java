7186215697edb9ab24283172a5c37c9de56cdb94
bitt <bitt@bit>
2017-11-21 13:49:05 +0900

    혁주 / mapper - TeamPlayerRecord - 팀 기록 매퍼 만듬

---
+++ baseball/src/main/java/org/bakara/dto/TeamPlayerRecordDTO.java
@@ -0,0 +1,31 @@
+package org.bakara.dto;
+
+import lombok.Data;
+
+@Data
+public class TeamPlayerRecordDTO {
+	String teamname ; 
+	Long war ; 
+	int game; 
+	int hs; 
+	int hn; 
+	int pt; 
+	int hit; 
+	int h2 ; 
+	int h3 ; 
+	int homerun; 
+	int bh ; 
+	int rbi; 
+	int sb ; 
+	int sf; 
+	int bb; 
+	int ddb; 
+	int ifb; 
+	int so ; 
+	int doubleout ;
+	int avg; 
+	int obp;
+	int sig ; 
+	int ops; 
+	
+}
--- baseball/src/main/java/org/bakara/mapper/TeamPlayerMapper.java
+++ baseball/src/main/java/org/bakara/mapper/TeamPlayerMapper.java
@@ -1,7 +1,9 @@
 package org.bakara.mapper;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import org.bakara.dto.TeamPlayerDTO;
+import org.bakara.dto.TeamPlayerRecordDTO;
 
 
 public interface TeamPlayerMapper {
@@ -9,7 +11,11 @@ public interface TeamPlayerMapper {
 
 	@Insert("insert into tbl_teamplayer (playerNum,playerName,teamName,position,birthday,HnW,school) values(#{playerNum},#{playerName},#{teamName},"
 			+ " #{position},#{birthday},#{hnW},#{school})")
-	public void registerTeamPlayer( TeamPlayerDTO dto);
+	public void registerTeamPlayer(TeamPlayerDTO dto);
+	
+	@Select("select * from tbl_teamrecord")
+	public void teamPlayerRecord(TeamPlayerRecordDTO dto)  ;
+	
 
-
+	
 }
